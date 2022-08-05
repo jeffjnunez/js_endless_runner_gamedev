ccf5a29cc14a93fc4cff562d0e4a1f4e9e03a241
Jeffrey Nunez <jeffjnunez@gmail.com>
2022-08-05 09:02:09 -0700

    add slider to adjust scroll speed

--- proj02/script.js
+++ proj02/script.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 
-let gameSpeed = 10;
+let gameSpeed = 5;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = '../assets/layer-1.png';
@@ -16,6 +16,13 @@ const backgroundLayer4 = new Image();
 backgroundLayer4.src = '../assets/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = '../assets/layer-5.png';
+
+const slider = document.getElementById('slider');
+const showGameSpeed = document.getElementById('showGameSpeed');
+slider.value = showGameSpeed.innerHTML = gameSpeed;
+slider.addEventListener('change', e => {
+    gameSpeed = showGameSpeed.innerHTML = e.target.value;
+});
 
 class Layer {
     constructor(image, speedModifier) {
@@ -50,27 +57,23 @@ class Layer {
     }
 }
 
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const layers = [layer1, layer2, layer3, layer4, layer5];
+
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    layer1.update();
-    layer1.draw();
-    layer2.update();
-    layer2.draw();
-    layer3.update();
-    layer3.draw();
-    layer4.update();
-    layer4.draw();
-    layer5.update();
-    layer5.draw();
+    layers.forEach(layer => {
+        layer.update();
+        layer.draw();
+    });
 
     requestAnimationFrame(animate);
 }
 
-const layer1 = new Layer(backgroundLayer1, 0.1);
-const layer2 = new Layer(backgroundLayer2, 0.2);
-const layer3 = new Layer(backgroundLayer3, 0.3);
-const layer4 = new Layer(backgroundLayer4, 0.4);
-const layer5 = new Layer(backgroundLayer5, 0.5);
-
-// animate();
+animate();
