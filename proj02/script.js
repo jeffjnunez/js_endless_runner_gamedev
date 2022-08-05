0161dd3e9ed0bd4af40efd14fb9a1c5681de49b5
Jeffrey Nunez <jeffjnunez@gmail.com>
2022-08-05 09:17:00 -0700

    refactor x pos update, run after page finish load

--- proj02/script.js
+++ proj02/script.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 
+// let gameFrame = 0;
 let gameSpeed = 5;
 
 const backgroundLayer1 = new Image();
@@ -17,63 +18,64 @@ backgroundLayer4.src = '../assets/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = '../assets/layer-5.png';
 
-const slider = document.getElementById('slider');
-const showGameSpeed = document.getElementById('showGameSpeed');
-slider.value = showGameSpeed.innerHTML = gameSpeed;
-slider.addEventListener('change', e => {
-    gameSpeed = showGameSpeed.innerHTML = e.target.value;
-});
-
-class Layer {
-    constructor(image, speedModifier) {
-        this.x = 0;
-        this.y = 0;
-        this.width = 2400;
-        this.height = 700;
-        this.x2 = this.width;
-        this.image = image;
-        this.speedModifier = speedModifier;
-        this.speed = gameSpeed * this.speedModifier;
-    }
-
-    update() {
-        this.speed = gameSpeed * this.speedModifier;
-
-        if (this.x <= -this.width) {
-            this.x = this.width + this.x2 - this.speed;
-        }
-
-        if (this.x2 <= -this.width) {
-            this.x2 = this.width + this.x - this.speed;
-        }
-
-        this.x = Math.floor(this.x - this.speed);
-        this.x2 = Math.floor(this.x2 - this.speed);
-    }
-
-    draw() {
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
-    }
-}
-
-const layer1 = new Layer(backgroundLayer1, 0.2);
-const layer2 = new Layer(backgroundLayer2, 0.4);
-const layer3 = new Layer(backgroundLayer3, 0.6);
-const layer4 = new Layer(backgroundLayer4, 0.8);
-const layer5 = new Layer(backgroundLayer5, 1);
-
-const layers = [layer1, layer2, layer3, layer4, layer5];
-
-function animate() {
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
-    layers.forEach(layer => {
-        layer.update();
-        layer.draw();
+window.addEventListener('load', () => {
+    const slider = document.getElementById('slider');
+    const showGameSpeed = document.getElementById('showGameSpeed');
+    slider.value = showGameSpeed.innerHTML = gameSpeed;
+    slider.addEventListener('change', e => {
+        gameSpeed = showGameSpeed.innerHTML = e.target.value;
     });
 
-    requestAnimationFrame(animate);
-}
+    class Layer {
+        constructor(image, speedModifier) {
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 700;
+            this.image = image;
+            this.speedModifier = speedModifier;
+            this.speed = gameSpeed * this.speedModifier;
+        }
 
-animate();
+        update() {
+            this.speed = gameSpeed * this.speedModifier;
+
+            if (this.x <= -this.width) {
+                this.x += this.width;
+            }
+
+            this.x = Math.floor(this.x - this.speed);
+
+            // Alternate technique which didn't work because it causes
+            // background to jump around suddenly when gameSpeed changes.
+            // this.x = Math.floor((gameFrame * this.speed) % this.width);
+        }
+
+        draw() {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+        }
+    }
+
+    const layer1 = new Layer(backgroundLayer1, 0.2);
+    const layer2 = new Layer(backgroundLayer2, 0.4);
+    const layer3 = new Layer(backgroundLayer3, 0.6);
+    const layer4 = new Layer(backgroundLayer4, 0.8);
+    const layer5 = new Layer(backgroundLayer5, 1);
+
+    const layers = [layer1, layer2, layer3, layer4, layer5];
+
+    function animate() {
+        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        layers.forEach(layer => {
+            layer.update();
+            layer.draw();
+        });
+        // gameFrame--;
+
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+});
