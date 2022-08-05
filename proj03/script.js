ed44880a297edd41e141dbcca15cbd4f9c5f9665
Jeffrey Nunez <jeffjnunez@gmail.com>
2022-08-05 14:55:20 -0700

    constrain enemy spawn within canvas give enemy1 jumpy random movement (bat)

--- proj03/script.js
+++ proj03/script.js
@@ -13,22 +13,20 @@ class Enemy {
     constructor() {
         this.image = new Image();
         this.image.src = '../assets/enemy1.png';
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.speed = (Math.random() * 4) - 2; // [-2, 2]
-        // this.speed = Math.floor((Math.random() * 4) - 2); // [-2, 2]
         this.spriteWidth = 293;
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.numFrames = 6;
         this.flapSpeed = Math.floor(Math.random() * 2 + 2);
     }
 
     update() {
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;
 
         if (gameFrame % this.flapSpeed === 0) {
             this.frame > this.numFrames - 2 ? this.frame = 0 : this.frame++;
