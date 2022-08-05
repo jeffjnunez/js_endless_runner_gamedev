9820284c6c26f81d45ff39f1e694301117e90b37
Jeffrey Nunez <jeffjnunez@gmail.com>
2022-08-05 14:51:17 -0700

    added bat sprite with varying animation play rate

--- proj03/script.js
+++ proj03/script.js
@@ -7,32 +7,54 @@ CANVAS_HEIGHT = canvas.height = 1000;
 
 const numEnemies = 20;
 const enemies = [];
+let gameFrame = 0;
 
 class Enemy {
     constructor() {
+        this.image = new Image();
+        this.image.src = '../assets/enemy1.png';
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
-        this.width = 100;
-        this.height = 100;
         this.speed = (Math.random() * 4) - 2; // [-2, 2]
         // this.speed = Math.floor((Math.random() * 4) - 2); // [-2, 2]
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.frame = 0;
+        this.numFrames = 6;
+        this.flapSpeed = Math.floor(Math.random() * 2 + 2);
     }
 
     update() {
         this.x += this.speed;
         this.y += this.speed;
+
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > this.numFrames - 2 ? this.frame = 0 : this.frame++;
+        }
+
     }
 
     draw() {
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(
+            this.image,
+            this.frame * this.spriteWidth,
+            0,
+            this.spriteWidth,
+            this.spriteHeight,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
     }
 }
 
 for (let i = 0; i < numEnemies; i++) {
     enemies.push(new Enemy());
 }
-
-console.log(enemies);
 
 const animate = () => {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -41,8 +63,9 @@ const animate = () => {
         enemy.update();
         enemy.draw();
     });
+    gameFrame++;
 
     requestAnimationFrame(animate);
 };
 
-// animate();
+animate();
