97c6ec9ced8a4e5638918b421c43e7804a1fc43d
Jeffrey Nunez <jeffjnunez@gmail.com>
2022-08-11 11:16:55 -0700

    proj04 - draw explosion sprite animation added sound effect array to support multiple spawned anims using canvas rotation

--- proj04/script.js
+++ proj04/script.js
@@ -9,62 +9,89 @@ let canvasPos = canvas.getBoundingClientRect();
 
 class Explosion {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
         this.image = new Image();
         this.image.src = '../assets/boom.png';
         this.frame = 0;
         this.numFrames = 5;
+        this.timer = 0;
         this.spriteWidth = 200;
         this.spriteHeight = 179;
-        this.width = this.spriteWidth / 2;
-        this.height = this.spriteHeight / 2;
+        this.width = this.spriteWidth * 0.7;
+        this.height = this.spriteHeight * 0.7;
+        this.x = x;
+        this.y = y;
+        this.angle = Math.random() * 6.2;
+        this.sound = new Audio();
+        this.sound.src = '../assets/boom6.wav';
     }
 
     update() {
-        this.frame++;
+        // Do nothing; position is not being updated currently.
+    }
+
+    updateAnim() {
+        if (this.frame === 0) {
+            this.sound.play();
+        }
+
+        this.timer++;
+
+        if (this.timer % 10 === 0) {
+            this.frame++;
+        }
     }
 
     draw() {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+
         ctx.drawImage(
             this.image,
             this.frame * this.spriteWidth,
             0,
             this.spriteWidth,
             this.spriteHeight,
-            this.x,
-            this.y,
+            0 - this.width / 2,
+            0 - this.height / 2,
             this.width,
             this.height
         );
+
+        ctx.restore();
     }
 }
 
-const boomTest = new Explosion(30, 70);
-// console.log(boomTest.image.src);
-// boomTest.draw();
+window.addEventListener('click', (e) => {
+    createAnimation(e);
+});
 
+// window.addEventListener('mousemove', (e) => {
+//     createAnimation(e);
+// });
 
+const createAnimation = e => {
+    let posX = e.x - canvasPos.left;
+    let posY = e.y - canvasPos.top;
+
+    explosions.push(new Explosion(posX, posY));
+};
+
+let numAnimLoops = 0;
 const animate = () => {
-    boomTest.update();
-    boomTest.draw();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    explosions.forEach((explosion, idx) => {
+        explosion.update();
+        explosion.updateAnim();
+        explosion.draw();
+
+        if (explosion.frame > 5) {
+            explosions.splice(idx, 1);
+        }
+    });
 
     requestAnimationFrame(animate);
+};
 
-    // console.log(boomTest.frame);
-}
-
-// animate();
-
-window.addEventListener('click', (e) => {
-    // console.log(e);
-    ctx.fillStyle = 'white';
-    const rectX = 40;
-    const rectY = 36;
-    ctx.fillRect(
-        e.x - canvasPos.x - rectX * 0.5,
-        e.y - canvasPos.y - rectY * 0.5,
-        rectX,
-        rectY
-    );
-});
+animate();
