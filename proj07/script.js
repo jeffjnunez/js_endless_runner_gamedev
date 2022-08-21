58cab6e5c650c334093bbfa6f944783d74175cf4
Jeffrey Nunez <jeffjnunez@gmail.com>
2022-08-21 12:02:18 -0700

    proj07 - starting inputHandler code, sprite anim

--- proj07/script.js
+++ proj07/script.js
@@ -8,18 +8,83 @@ window.addEventListener('load', () => {
 
     ctx.font = '40px Impact';
 
+    const keysToRegister = [
+        'ArrowDown',
+        'ArrowUp',
+        'ArrowLeft',
+        'ArrowRight',
+    ];
 
     class InputHandler {
         constructor() {
             this.keys = []; // all keys currently being pressed
-            window.addEventListener('keydown', (e) => {
-                console.log(e);
+
+            window.addEventListener('keydown', e => {
+                if (keysToRegister.includes(e.key) &&
+                    this.keys.indexOf(e.key) === -1)
+                {
+                    this.keys.push(e.key);
+                }
+            });
+
+            window.addEventListener('keyup', e => {
+                if (keysToRegister.includes(e.key)) {
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
             });
         }
     }
 
     class Player {
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.image = playerImage;
+            this.spriteWidth = 200;
+            this.spriteHeight = 200;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            this.x = 10;
+            this.y = this.gameHeight - this.height - 15;
+            this.xSpeed = 0.2;
+            this.frame = 0;
+            this.numFrames = 9;
+            this.frameInterval = 60;
+            this.frameTime = 0;
+            this.row = 0;
+            console.log(this.image);
+        }
 
+        update(dT) {
+            this.x += this.xSpeed * dT;
+
+            this.frameTime += dT;
+            if (this.frameTime > this.frameInterval) {
+                this.frameTime -= this.frameInterval;
+
+                this.frame++;
+                if (this.frame > this.numFrames - 1) {
+                    this.frame = 0;
+                }
+            }
+        }
+
+        draw(context) {
+            // context.fillStyle = 'white';
+            // context.fillRect(this.x, this.y, this.width, this.height);
+
+            context.drawImage(
+                this.image,
+                this.frame * this.spriteWidth,
+                this.row * this.spriteHeight,
+                this.spriteWidth,
+                this.spriteHeight,
+                this.x,
+                this.y,
+                this.width,
+                this.height
+            );
+        }
     }
 
     class Background {
@@ -38,18 +103,22 @@ window.addEventListener('load', () => {
 
     }
 
-    // let lastTimestamp = 0;
+    let lastTimestamp = 0;
     const animate = (timestamp) => {
-    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    //     const dT = timestamp - lastTimestamp;
-    //     lastTimestamp = timestamp;
+        const dT = timestamp - lastTimestamp;
+        lastTimestamp = timestamp;
     //     // some code
 
-    //     game.update(dT);
-    //     game.draw();
-    //     requestAnimationFrame(animate);
+        player.update(dT);
+        player.draw(ctx);
+        requestAnimationFrame(animate);
     };
 
     const input = new InputHandler();
+    const player = new Player(canvas.width, canvas.height);
+    // animate(0);
+
+
 });
